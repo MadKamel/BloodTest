b3e471f8008002289e332adb38d6600d82cb111d
Eric Rustrum <48301203+MadKamel@users.noreply.github.com>
2022-01-21 15:17:39 -0800

    Fixed fire damage bug.
    Now, bt_core:fire should provide damage to the player.

--- mods/bt_core/fire.lua
+++ mods/bt_core/fire.lua
@@ -2,7 +2,7 @@ local register_node = minetest.register_node
 
 register_node('bt_core:fire', {
     description = 'Fire Starter',
-    damage_per_second = '3',
+    damage_per_second = 3,
     drawtype = 'firelike',
     paramtype = 'light',
     walkable = false,
