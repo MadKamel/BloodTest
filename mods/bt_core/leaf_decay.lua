a4cf143a1aface212f933bda6b76a17f65b274fa
Eric Rustrum <48301203+MadKamel@users.noreply.github.com>
2022-01-23 16:36:02 -0800

    Fixed some bugs.

--- mods/bt_core/leaf_decay.lua
+++ mods/bt_core/leaf_decay.lua
@@ -1,10 +1,10 @@
 local distance = 3
 
 minetest.register_abm({
-	label = "Remove lonely juniper leaves.",
+	label = "Remove lonely plum leaves.",
 	nodenames = {"bt_core:plum_leaves"},
-	interval = 3,
-	chance = 1,
+	interval = 1,
+	chance = 3,
 	action = function(pos, node, active_object_count, active_object_count_wider)
 		if minetest.find_node_near(pos, distance, { "bt_core:plum_trunk" }) == nil then
 			minetest.set_node(pos, {name = "air"})
@@ -13,10 +13,10 @@ minetest.register_abm({
 })
 
 minetest.register_abm({
-	label = "Remove lonely holly leaves.",
+	label = "Remove lonely peach leaves.",
 	nodenames = {"bt_core:peach_leaves"},
-	interval = 3,
-	chance = 1,
+	interval = 1,
+	chance = 3,
 	action = function(pos, node, active_object_count, active_object_count_wider)
 		if minetest.find_node_near(pos, distance, { "bt_core:peach_trunk" }) == nil then
 			minetest.set_node(pos, {name = "air"})
